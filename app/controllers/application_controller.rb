f4119d2b2e6c41b3011028ee544d783bf19d6263
Eric Fiorani <efiorani305@gmail.com>
2018-08-31 13:29:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,17 @@ class ApplicationController < Sinatra::Base
   get '/recipes/new' do
     erb :new
   end
+
+  post '/recipes' do
+    @recipes = Recipe.create(:name => params[:name], :ingredients => params[:ingredients],
+    :cook_time => params[:cook_time])
+
+    redirect to '/recipes/#{@recipes.id}'
+  end
+
+  get 'recipes/:id' do
+    @recipes = Article.find_by_id(params[:id])
+
+    erb :show
+  end
 end
